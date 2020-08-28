b84dc3abd45abb41de05c3d3b3a7df97a57142fc
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-08-28 16:38:26 +0545

    tring to working in npm

---
+++ dist/index.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "covertBSToAD", {
+  enumerable: true,
+  get: function get() {
+    return _helper.covertBSToAD;
+  }
+});
+Object.defineProperty(exports, "covertADToBS", {
+  enumerable: true,
+  get: function get() {
+    return _helper.covertADToBS;
+  }
+});
+exports["default"] = void 0;
+
+var _component = _interopRequireDefault(require("./src/component.v3"));
+
+var _helper = require("./src/helper");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = _component["default"];
+exports["default"] = _default;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import useDatepicker from './component.v3';
+import useDatepicker from './src/component.v3';
 
-import { covertBSToAD, covertADToBS } from './helper';
+import { covertBSToAD, covertADToBS } from './src/helper';
 
 export { covertADToBS, covertBSToAD };
 
